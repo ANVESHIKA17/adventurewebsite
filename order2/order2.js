68b5b632cfeec338f5feb80065f329c88f282992
ANVESHIKA17 <87799213+ANVESHIKA17@users.noreply.github.com>
2022-11-30 01:47:14 +0530

    first commit

---
+++ order2/order2.js
@@ -0,0 +1,14 @@
+function callvalue(){
+
+
+    let username= document.getElementById('username').value;
+    console.log(uname);
+    let mail= document.getElementById('mail').value;
+    console.log(mail);
+    let phn= document.getElementById('phn').value;
+    console.log(phn);
+   
+    let items= document.querySelector('#items').value;
+    console.log(items);
+    document.writeln("Thank You "+username+"<br>"+"For purchasing "+  items +" products, Expect Delivery in 5-4 WORKING DAYS! " );
+    }
